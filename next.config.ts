7d96b2544be205f686ca5f4a21a32445464b9626
Mecster09 <gkp2105@gmail.com>
2024-12-17 16:03:31 +0000

    Blank start

---
+++ next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+import withPWA from "next-pwa";
+
+const nextConfig: NextConfig = withPWA({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+})({
+  /* config options here */
+});
+
+export default nextConfig;
